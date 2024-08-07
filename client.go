22e322fbaa657de1e61c8f1ccc0da53cb3dd4296
9ziggy9 <davidarogers@protonmail.com>
2024-08-07 14:48:27 -0700

    client disconnection tied to context finally

--- client.go
+++ client.go
@@ -16,8 +16,9 @@ type wsMsg struct {
 }
 
 type wsSession struct {
-	conn *websocket.Conn;
-	ctx   context.Context;
+	conn     *websocket.Conn;
+	ctx       context.Context;
+	cancelCtx context.CancelFunc
 }
 
 // BEGIN: client methods
@@ -33,7 +34,7 @@ func (me *wsClient) emitMsgs() {
 			err := me.session.conn.Write(me.session.ctx, msg.msg_t, msg.raw)
 			if err != nil {
 				ServerLog(
-					ERROR, "failed to send message to client %d:\n  -> %v",
+					INFO, "failed to send message to client %d:\n  -> %v",
 					me.id, err,
 				)
 				return
@@ -42,14 +43,20 @@ func (me *wsClient) emitMsgs() {
 		}
 	}
 }
-func (me *wsClient) connect(session *wsSession, rm *wsRoom) error {
+func (me *wsClient) connect(
+	session *wsSession,
+	rm *wsRoom,
+) websocket.StatusCode {
 	for {
 		select {
 		case <-session.ctx.Done():
-			return nil
+			return websocket.StatusNormalClosure
 		default:	
 			msg_t, msg, err := session.conn.Read(session.ctx)
-			if err != nil { return err }
+			if err != nil {
+				me.session.cancelCtx()
+				return websocket.CloseStatus(err)
+			}
 			rm.broadcast(wsMsg{msg_t, msg, me.id})
 			ServerLog(INFO, "msg :: (client: %d) : %s", me.id, msg[:len(msg) - 1])
 		}
@@ -76,6 +83,7 @@ func (rm *wsRoom) addClient(session *wsSession) *wsClient {
 	return newClient
 }
 func (rm *wsRoom) removeClient(clientId uint64) {
+	defer ServerLog(INFO, "disconnecting client %d", clientId)
 	rm.mtx.Lock()
 	defer rm.mtx.Unlock()
 	delete(rm.clients, clientId)
@@ -97,6 +105,7 @@ func (prov *wsRoomProvider) roomExists(rmId uint64) bool {
 	_, ok := prov.rooms[rmId]
 	return ok
 }
+
 func (prov *wsRoomProvider) createRoom(rmId uint64) *wsRoom {
 	prov.rooms[rmId] = &wsRoom{
 		clientCount: 0,
@@ -130,25 +139,33 @@ func routesWS() *http.ServeMux {
 		if err != nil {
 			ServerLog(ERROR, "failed to accept WS connection:\n  -> %v", err)
 		}
-		defer conn.CloseNow()
+		defer conn.CloseNow() // no-op but just in case
 
-		ctx, cancel := context.WithCancel(r.Context())
-		defer cancel()
+		ctx, cancelCtx := context.WithCancel(r.Context())
+		defer cancelCtx() // no-op but just in case
 
 		if !WS_ROOMS.roomExists(rmId)  { WS_ROOMS.createRoom(rmId) }
 
-		client := WS_ROOMS.rooms[rmId].addClient(&wsSession{conn, ctx})
+		client := WS_ROOMS.rooms[rmId].addClient(&wsSession{
+			conn, ctx, cancelCtx,
+		})
 
 		var wg sync.WaitGroup
 		wg.Add(2)
 			go func() {
 				defer wg.Done()
 				defer WS_ROOMS.rooms[rmId].removeClient(client.id)
-				defer ServerLog(INFO, "disconnecting client %d", client.id)
-				client.connect(&wsSession{conn, ctx}, WS_ROOMS.rooms[rmId])
+				close_status := client.connect(
+					&wsSession{conn, ctx, cancelCtx},
+					WS_ROOMS.rooms[rmId],
+				);
+				ServerLog(INFO, "client disconnection code: %d", close_status)
 			}()
 
-			go func() { defer wg.Done(); client.emitMsgs() }()
+			go func() {
+				defer wg.Done()
+				client.emitMsgs()
+			}()
 		wg.Wait()
 
 		ServerLog(
